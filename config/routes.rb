abe5cb870f2ead8ce42efe8d9323c191aa759083
ueslei neves <unp1210@gmail.com>
2020-08-12 22:11:51 -0300

    Fechando rotas das categorias carro e filial

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  resources :car_categories
-  resources :subsidiaries
+  resources :car_categories, only: [:index, :show, :new, :create]
+  resources :subsidiaries, only: [:index, :show, :new, :create]
 end
