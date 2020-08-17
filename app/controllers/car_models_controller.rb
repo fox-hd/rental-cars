ca24fca6ad5dced11b441020b79bd78885bc3550
ueslei neves <unp1210@gmail.com>
2020-08-17 16:48:27 -0300

    Registro de carro feito com sucesso

--- app/controllers/car_models_controller.rb
+++ app/controllers/car_models_controller.rb
@@ -7,4 +7,22 @@ class CarModelsController < ApplicationController
     @car_model = CarModel.find(params[:id])
   end
 
+  def new
+    @car_model = CarModel.new
+    @car_categories = CarCategory.all
+  end
+
+  def create
+  @car_model = CarModel.new(car_model_params)
+  @car_model.save
+  redirect_to @car_model
+  end
+
+  private
+
+  def car_model_params
+    params.require(:car_model).permit(:name, :year,:manufacturer,
+    :motorization, :fuel_type, :car_category_id)
+  end
+
 end
