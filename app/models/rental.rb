cc46f6bf189561e8b33a2d52727e307e2a50c24e
ueslei neves <unp1210@gmail.com>
2020-08-25 16:08:25 -0300

    Gerar o token na locação

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -5,10 +5,19 @@ class Rental < ApplicationRecord
 
   validates :start_date, :end_date, presence:true
 
+  before_create :generate_token
+
   def total
     number_of_days_rental = end_date - start_date
     daily_rate = car_category.daily_price
 
     number_of_days_rental * daily_rate
   end
+
+  private
+
+  def generate_token
+    self.token = SecureRandom.alphanumeric(6).upcase
+  end
+
 end
--- spec/models/rental_spec.rb
+++ spec/models/rental_spec.rb
@@ -14,6 +14,25 @@ RSpec.describe Rental, type: :model do
       expect(rental.errors[:car_category]).to include('é obrigatório(a)')
       expect(rental.errors[:user]).to include('é obrigatório(a)')
     end
+    
+    context 'token' do
+      it 'generate on create' do
+        client = Client.create!(name: 'Fulano Sicrano', cpf: '512.129.580-47', 
+                                email: 'test@client.com')
+        car_category = CarCategory.create!(name: 'A', car_insurance: 100, 
+                                           daily_rate: 100, third_party_insurance: 100)
+        user = User.create!(name: 'Lorem Upsum', email: 'lorem@ipsum.com',
+                            password: '12345678')
+        rental = Rental.new(start_date: Date.current, end_date: 1.day.from_now,
+                            client: client, car_category: car_category, user: user)
 
+        rental.save!  
+        rental.reload
+
+        expect(rental.token).to be_present
+        expect(rental.token.size).to eq(6)
+        expect(rental.token).to match(/^[A-Z0-9]+$/)
+      end
+    end
   end
 end
