18bd04c9335bc8563409bb465576ca8be54e88c6
ueslei neves <unp1210@gmail.com>
2020-09-22 14:11:18 -0300

    Factories do factory bot

---
+++ spec/factories/cars.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :car do
+    license_plate {'ABC1234'}
+    mileage { 1000 }
+    color {'Vermelho'}
+    status { :available }
+    car_model
+    subsidiary
+  end
+end
---
+++ spec/factories/subsidiaries.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :subsidiary do
+    name {'Unidas'}
+    cnpj {'63.463.524/0001-39'}
+    address {'Rua Santiago'}
+  end
+end
